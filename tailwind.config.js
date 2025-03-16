c9dc2df097f22360c813dc0bd2acdfc0d352d823
biyash-1 <sthaalish39@gmail.com>
2025-03-16 23:25:59 +0545

    config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.mjs
+export default {
+    darkMode: ['class', '.my-app-dark'],
+    content: [
+      "./index.html",
+      "./src/**/*.{vue,js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
