ca423a021e34a682c7a44068116b6fc1f3e68f78
biyash-1 <sthaalish39@gmail.com>
2025-03-17 22:52:08 +0545

    app.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import {RouterView}   from 'vue-router'
+
+
+import Toast from 'primevue/toast';
+
+import { useCounterStore } from './stores/counter';
+
+const counter = useCounterStore();
+
+</script>
+
+<template>
+ <Navbar/>
+
+<Toast/>
+
+  <RouterView/>
+</template>
